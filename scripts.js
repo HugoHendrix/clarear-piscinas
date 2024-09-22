0fdeb619c3e60c16285933474549eab6ccd197cf
Hugo Hendrix Carvalho Rosa <75282590+HugoHendrix@users.noreply.github.com>
2024-09-22 19:08:47 -0300

    adicionado depoimentos

--- scripts.js
+++ scripts.js
@@ -33,3 +33,27 @@ $(document).ready(function () {
         event.stopPropagation();
     });
 });
+
+
+$(document).ready(function(){
+    $("#owl-depoimentos").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 1
+          },
+          1000: {
+              items: 2
+          }
+      }
+    });
+  });
+  
